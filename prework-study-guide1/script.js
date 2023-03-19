e86e95fdb50b898d19e6bffb874d6a750f020dec
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-03-19 17:02:20 -0500

    Update script.js
    Created for loop, for array of topics

---
+++ prework-study-guide1/script.js
@@ -0,0 +1,26 @@
+
+
+var topics = ["Html", "CSS", "Git", "JavaScript"];
+console.log(topics[0]);
+
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+
+
+/*
+if (topic === 'HTML') {
+    console.log("Lets study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets study CSS!");
+} else if (topic === 'GIT') {
+    console.log("Lets study GIT!");
+} else if (topic === 'JavaScript') {
+    console.log ("lets study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
+
+
